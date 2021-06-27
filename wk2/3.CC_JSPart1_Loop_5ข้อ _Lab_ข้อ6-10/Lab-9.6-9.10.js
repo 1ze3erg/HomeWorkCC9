a472521e142615d63bd11c7e7fcd5cd4cffdb076
izeberg <skizeza666@gmail.com>
2021-06-27 20:41:34 +0700

    Homework Week 2

---
+++ wk2/3.CC_JSPart1_Loop_5ข้อ _Lab_ข้อ6-10/Lab-9.6-9.10.js
@@ -0,0 +1,71 @@
+function isNumber(num) {
+    // initail value
+    let isZero = false;
+    // check zero number
+    if (+num === 0) {
+        if (num === null) isZero = false;
+        else if (num[0] === "0" || num.trim() !== "") isZero = true;
+        else isZero = false;
+    } else isZero = false;
+    // check number or NaN
+    const isNumber = !!+num ? true : isZero ? true : false
+    return isNumber;
+}
+
+// Lab 9.6
+// function draw(n) {
+//     let sum = "";
+//     for (let i = 1; i <= n; i++) {
+//         sum += "*"
+//         console.log(sum);
+//     }
+// }
+// draw(4);
+
+// Lab 9.7
+// for (let i = 2; i <= 12; i++) {
+//     for (let j = 1; j <= 12; j++) {
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+//     console.log("----------------------------");
+// }
+
+// Lab 9.8
+// let sum = 0;
+// while (true) {
+//     let num = +prompt("input num");
+//     if (!num || num < 0) {
+//         break
+//     }
+//     sum += num;
+// }
+// console.log(`sum = ${sum}`);
+
+// Lab 9.9
+// let player1Num = prompt("player1 input num");
+// let count = 0;
+// while (isNumber(player1Num) && player1Num >= 1 && player1Num <= 99) {
+//     count++;
+//     let player2Num = prompt("player2 guess num");
+//     if (isNumber(player2Num) && +player2Num === +player1Num) {
+//         alert("ถูกต้อง");
+//         alert(`ยินดีด้วยทายถูกแล้ว คุณเล่นทั้งหมด ${count} รอบ`);
+//         break
+//     } else if (isNumber(player2Num) && +player2Num >= +player1Num) {
+//         alert("มากกว่าคำตอบ");
+//     } else if (isNumber(player2Num) && +player2Num <= +player1Num) {
+//         alert("น้อยกว่าคำตอบ");
+//     }
+// }
+
+// Lab 9.10
+// for (let i = 1; i <= 100; i++) {
+//     let round = 0
+//     for (let j = 1; j <= i; j++) {
+//         if (i % j === 0) round++;
+//     }    
+//     if (round === 2) {
+//         console.log(i);
+//         console.log("-------------------------")
+//     }
+// }
